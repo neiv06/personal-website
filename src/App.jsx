6007c528160b4eef6cc223273467d27a4f732c4c
neiv06 <80381222+neiv06@users.noreply.github.com>
2026-01-07 18:34:05 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -390,8 +390,7 @@ const Portfolio = () => {
               </h1>
               
               <h2 className="text-xl md:text-2xl text-white-300 mb-8 fade-in-up-delay-1">
-                
-                 <span className="font-bold mx-2 text-md">✦</span> Researcher <span className="font-bold mx-2 text-md">✦</span> UCLA Student
+                Software Engineer <span className="font-bold mx-2 text-md">✦</span> Researcher <span className="font-bold mx-2 text-md">✦</span> UCLA Student
               </h2>
               
               <p className="text-base md:text-lg text-gray-300 mb-8 max-w-2xl mx-auto fade-in-up-delay-2">
