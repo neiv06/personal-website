0474c8f257d5d0cde2ec870d7f15002ce65c1b5f
neiv06 <80381222+neiv06@users.noreply.github.com>
2026-01-07 18:05:34 -0800

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,10 @@ import { Github, Linkedin, Mail, ExternalLink, ChevronDown, Terminal, Code, Brie
 import profileImage from './images/headshot.JPG';
 import cudaFireImage from './images/CudaFire.png';
 import bruinMarketImage from './images/BruinMarket.png';
+import resume from './Neiv_Gupta_Resume.pdf';
 import yumImage from './images/YUM.png';
 import stairmastersImage from './images/Stairmasters.png';
 import './App.css';
-
-const resume = '/Neiv_Gupta_Resume.pdf';
 
 const Portfolio = () => {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
